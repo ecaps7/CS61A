0c6943289884551becb136ab22d926cbedae0c23
ecaps7 <ecaps7@126.com>
2024-01-22 14:29:55 +0800

    hw08, Jan.22, 2024

---
+++ Homework/hw08/hw08.scm
@@ -0,0 +1,24 @@
+(define (ascending? s)
+    (if (or (null? s) (null? (cdr s)))
+        #t
+        (if (<= (car s) (car (cdr s)))
+            (ascending? (cdr s))
+            #f)))
+
+(define (my-filter pred s)
+    (if (null? s)
+        nil
+        (if (pred (car s))
+        (append (list(car s)) (my-filter pred (cdr s)))
+        (my-filter pred (cdr s)))))
+
+(define (interleave lst1 lst2)
+    (cond 
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) 
+                    (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
+
+(define (no-repeats s)
+    (if (null? s) s
+        (cons (car s) (no-repeats (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
